82ec05e9f969b9218aac9773502464ef9686c7f6
masakih <masakih@users.sourceforge.jp>
2018-03-24 17:39:44 +0900

    Unit testを追加

--- DoutakuTests/DoutakuTests.swift
+++ DoutakuTests/DoutakuTests.swift
@@ -7,30 +7,142 @@
 //
 
 import XCTest
-@testable import Doutaku
+@testable import DoutakuApp
+
 
 class DoutakuTests: XCTestCase {
     
+    let testName = "NameTest"
+    let testId = 100_000
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
+        let model = Model.oneTimeEditor()
+        
+        model.sync {
+            
+            let p = NSPredicate(format: "%K == %@", "name", testName)
+            
+            do {
+                let persons = try model.objects(of: Person.entity, sortDescriptors: nil, predicate: p)
+                
+                persons.forEach(model.delete)
+                
+            } catch {
+                XCTFail("Caught Exception. \(error)")
+            }
+        }
+        model.save()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        let model = Model.oneTimeEditor()
+        
+        model.sync {
+            
+            let p = NSPredicate(format: "%K == %@", "name", testName)
+            
+            do {
+                let persons = try model.objects(of: Person.entity, sortDescriptors: nil, predicate: p)
+                
+                persons.forEach(model.delete)
+                
+            } catch {
+                XCTFail("Caught Exception. \(error)")
+            }
+        }
+        model.save()
+        
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInsertAndFetch() {
+        
+        let model = Model.oneTimeEditor()
+        
+        model.sync {
+            let person = model.insertNewObject(for: Person.entity)
+            person?.name = testName
+            person?.identifier = testId
+        }
+        model.save()
+        
+        let model2 = Model.oneTimeEditor()
+        model2.sync {
+            
+            let p = NSPredicate(format: "%K == %@", "name", testName)
+            
+            do {
+                let persons = try model2.objects(of: Person.entity, sortDescriptors: nil, predicate: p)
+                
+                XCTAssertFalse(persons.isEmpty)
+                
+                guard let person = persons.first else {
+                    
+                    XCTFail("Can not get Person from person's array")
+                    return
+                }
+                
+                guard let name = person.name else {
+                    
+                    XCTFail("Person Name is nil")
+                    return
+                }
+                
+                XCTAssertEqual(name, testName)
+                
+                XCTAssertEqual(person.identifier, testId)
+            
+            } catch {
+                XCTFail("Caught Exception. \(error)")
+            }
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSync() {
+        
+        let model = Model.oneTimeEditor()
+        
+        model.sync {
+            let person = model.insertNewObject(for: Person.entity)
+            person?.name = testName
+            person?.identifier = testId
         }
+        model.save()
+        
+        guard let name = Model.default.person(of: testName)?.name else {
+            
+            XCTFail("Can not get Person by name)")
+            return
+        }
+        
+        XCTAssertTrue(name == testName)
+        
+    }
+    
+    func testExcahnge() {
+        
+        let model = Model.oneTimeEditor()
+        
+        model.sync {
+            let person = model.insertNewObject(for: Person.entity)
+            person?.name = testName
+            person?.identifier = testId
+        }
+        model.save()
+        
+        guard let person = Model.default.person(of: testName) else {
+            
+            XCTFail("Can not get Person by name)")
+            return
+        }
+        
+        let model2 = Model.oneTimeEditor()
+        let person2 = model2.exchange(person)
+        XCTAssertNotNil(person2)
     }
     
 }

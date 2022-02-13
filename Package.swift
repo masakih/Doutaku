fbf9dbf438a3047b2b4ce04ec350eb9710e8edff
masakih <masakih@users.sourceforge.jp>
2022-02-13 22:05:56 +0900

    Package.swiftを修正

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Doutaku",
+    defaultLocalization: "ja",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -22,8 +23,13 @@ let package = Package(
             name: "Doutaku",
             dependencies: [],
             path: "Sources"),
-        .testTarget(
-            name: "DoutakuTests",
-            dependencies: ["Doutaku"])
+//        .target(
+//            name: "DoutakuApp",
+//            dependencies: ["Doutaku"],
+//            path: "DoutakuApp"),
+//        .testTarget(
+//            name: "DoutakuTests",
+//            dependencies: ["DoutakuApp"],
+//            path: "Tests")
     ]
 )
